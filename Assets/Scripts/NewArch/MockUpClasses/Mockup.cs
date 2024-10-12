6938e3cffffaa51bf6e2235c9110dab5fe53ae3a
TheBretonnian <tomas.macias@vector.com>
2024-10-12 15:43:10 +0200

    MockUp classes to avoid syntax error in c# project

---
+++ Assets/Scripts/NewArch/MockUpClasses/Mockup.cs
@@ -0,0 +1,36 @@
+public abstract class MonoBehaviour{
+    public bool enabled = true;
+}
+
+public abstract class ScriptableObject{}
+
+public class Vector3{
+    public float x;
+    public float y;
+    public float z;
+}
+
+public class Vector2{
+    public float x;
+    public float y;
+}
+
+public class Vector2Int{
+    public int x;
+    public int y;
+
+    public Vector2Int(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public static float Distance(Vector2Int orig, Vector2Int dest){ return 0.0f;}
+}
+
+public class Camera{
+    public static Camera main;
+}
+
+public class Ray{}
+
+public class RaycastHit{}
