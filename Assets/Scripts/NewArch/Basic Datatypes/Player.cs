0a17e48b13ef431442ca5324d94031b292131795
TheBretonnian <tomasmacias@outlook.com>
2024-10-24 22:13:30 +0200

    Small fixes and explanations

--- Assets/Scripts/NewArch/Basic Datatypes/Player.cs
+++ Assets/Scripts/NewArch/Basic Datatypes/Player.cs
@@ -6,17 +6,17 @@ using UnityEngine;
 public class Player
 {
 
-    [SerializeField] private int number;
+    [SerializeField] private int teamNumber;
     [SerializeField] private Color color;
     [SerializeField] private bool isHuman;
 
-    public int Number { get => number;}
+    public int Number { get => teamNumber;}
     public Color GetColor { get => color;}
     public bool IsHuman { get => isHuman;}
      
-    public Player(int number, Color color, isHuman = false)
+    public Player(int teamNumber, Color color, bool isHuman = false)
     {
-        this.number = number;
+        this.teamNumber = teamNumber;
         this.color = color;
         this.isHuman = isHuman;
     }
