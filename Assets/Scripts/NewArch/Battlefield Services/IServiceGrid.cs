ccc7c441dae0e5f213b3b83214ec14ecfa034c91
TheBretonnian <tomasmacias@outlook.com>
2024-10-24 22:12:59 +0200

    Add prototype interface for Battlefield Services

---
+++ Assets/Scripts/NewArch/Battlefield Services/IServiceGrid.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IServiceGrid
+{
+    Range GetRange(ITile origin, int distance);
+    Range GetRangeWalkable(ITile origin, int distance);
+    Range GetRangeWithLoS(ITile origin, int distance);
+
+    //Units
+
+    HashSet<IUnit> GetUnitsInRange(Range tiles);
+    HashSet<IUnit> GetUnitsInRange(ITile origin, int distance);
+
+    HashSet<IUnit> GetEnemiesInSet(IUnit referenceUnit, HashSet<IUnit> units);
+    HashSet<IUnit> GetAlliesInSet(IUnit referenceUnit, HashSet<IUnit> units);
+
+    //Transformer
+    ITile GetTileFromUnit(IUnit unit);
+    IUnit GetUnitFromTile(ITile tile);
+
+    //Line methods
+    int GetDistance(ITile orig, ITile dest);
+    int GetDistance(IUnit fromUnit, IUnit toUnit);
+    Range GetLineOfTiles(ITile orig, ITile dest);
+
+    //Pathfinding
+    bool HasPath(ITile orig, ITile dest, IUnit movingUnit = null);
+    Range GetPath(ITile orig, ITile dest, IUnit movingUnit = null);
+
+    //Visuals
+    void HighlightRange(Range range, Color color);
+    //return index of Outline as int (Object Pool)
+    int OutlineRange(Range range, Color color, int lineType = 0); //lineType optional -> eventually use Enum in concrete class
+
+    //Cover
+    bool HasCover(IUnit attacker, IUnit defender);
+    float GetNormalizedCover(IUnit attacker, IUnit defender); //Returns [0..1]
+    bool GetNormalizedCover(IUnit attacker, IUnit defender, out float cover);
+
+    //Line of Sight
+    bool HasLos(IUnit attacker, IUnit defender);
+    bool HasLos(ITile orig, ITile dest);
+
+    //Services:
+    // + ServiceGrid
+    // + ServiceGridVisual
+    // + ServicePathfinding
+    // + ServiceLoSandCover
+
+}
