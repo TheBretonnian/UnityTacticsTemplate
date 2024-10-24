c8c888fe615e986e6097d394c623e64851056938
TheBretonnian <tomasmacias@outlook.com>
2024-10-24 22:35:38 +0200

    Explanation

--- Assets/Scripts/NewArch/Global Interfaces/ITarget.cs
+++ Assets/Scripts/NewArch/Global Interfaces/ITarget.cs
@@ -1,5 +1,6 @@
 
-//ITarget is used to abstract a target for an ability: either a tile or a unit
+//ITarget is used to abstract a target for an ability: 
+//typically either a tile or a unit
 public interface ITarget  
 {
 
