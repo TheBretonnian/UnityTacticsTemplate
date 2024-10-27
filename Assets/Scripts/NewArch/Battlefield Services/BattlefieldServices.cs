8113cd16cda8ec2f98cea48cd8ebcb11bc90172b
TheBretonnian <tomasmacias@outlook.com>
2024-10-27 21:43:04 +0100

    Restructure the code

--- Assets/Scripts/NewArch/Battlefield Services/BattlefieldServices.cs
+++ Assets/Scripts/NewArch/Battlefield Services/BattlefieldServices.cs
@@ -1,77 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattlefieldServices : IServiceGrid
+public class BattlefieldServices : IServiceGrid, IServicePathfinding, IServiceLoSandCover, IServiceGridVisual
 {
     public BattlefieldServices()
     {
     }
-
-    public HashSet<IUnit> GetAlliesInSet(IUnit referenceUnit, HashSet<IUnit> units)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public int GetDistance(ITile orig, ITile dest)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public int GetDistance(IUnit fromUnit, IUnit toUnit)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public HashSet<IUnit> GetEnemiesInSet(IUnit referenceUnit, HashSet<IUnit> units)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Range GetLineOfTiles(ITile orig, ITile dest)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public float GetNormalizedCover(IUnit attacker, IUnit defender)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public bool GetNormalizedCover(IUnit attacker, IUnit defender, out float cover)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Range GetPath(ITile orig, ITile dest, IUnit movingUnit = null)
-    {
-        throw new System.NotImplementedException();
-    }
-
+    //Basic Service
     public Range GetRange(ITile origin, int distance)
     {
         throw new System.NotImplementedException();
     }
-
-    public Range GetRangeWalkable(ITile origin, int distance)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Range GetRangeWithLoS(ITile origin, int distance)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public ITile GetTileFromUnit(IUnit unit)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public IUnit GetUnitFromTile(ITile tile)
-    {
-        throw new System.NotImplementedException();
-    }
-
+    //Unit related methods
     public HashSet<IUnit> GetUnitsInRange(Range tiles)
     {
         throw new System.NotImplementedException();
@@ -81,34 +21,93 @@ public class BattlefieldServices : IServiceGrid
     {
         throw new System.NotImplementedException();
     }
+    public HashSet<IUnit> GetAlliesInSet(IUnit referenceUnit, HashSet<IUnit> units)
+    {
+        throw new System.NotImplementedException();
+    }
+    public HashSet<IUnit> GetEnemiesInSet(IUnit referenceUnit, HashSet<IUnit> units)
+    {
+        throw new System.NotImplementedException();
+    }
 
+    //Line methods
+    public int GetDistance(ITile orig, ITile dest)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int GetDistance(IUnit fromUnit, IUnit toUnit)
+    {
+        throw new System.NotImplementedException();
+    }
+    public Range GetLineOfTiles(ITile orig, ITile dest)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //Transformers
+    public ITile GetTileFromUnit(IUnit unit)
+    {
+        throw new System.NotImplementedException();
+    }
+    public IUnit GetUnitFromTile(ITile tile)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //Pathfinding
+    public Range GetRangeWalkable(ITile origin, int distance)
+    {
+        throw new System.NotImplementedException();
+    }
+    public bool HasPath(ITile orig, ITile dest, IUnit movingUnit = null)
+    {
+        throw new System.NotImplementedException();
+    }
+    public Range GetPath(ITile orig, ITile dest, IUnit movingUnit = null)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //LoS and Cover
+    public Range GetRangeWithLoS(ITile origin, int distance)
+    {
+        throw new System.NotImplementedException();
+    }
     public bool HasCover(IUnit attacker, IUnit defender)
     {
         throw new System.NotImplementedException();
     }
-
+    public float GetNormalizedCover(IUnit attacker, IUnit defender)
+    {
+        throw new System.NotImplementedException();
+    }
+    public bool GetNormalizedCover(IUnit attacker, IUnit defender, out float cover)
+    {
+        throw new System.NotImplementedException();
+    }
     public bool HasLos(IUnit attacker, IUnit defender)
     {
         throw new System.NotImplementedException();
     }
-
     public bool HasLos(ITile orig, ITile dest)
     {
         throw new System.NotImplementedException();
     }
 
-    public bool HasPath(ITile orig, ITile dest, IUnit movingUnit = null)
-    {
-        throw new System.NotImplementedException();
-    }
-
+    //Grid Visuals
     public void HighlightRange(Range range, Color color)
     {
         throw new System.NotImplementedException();
     }
-
     public int OutlineRange(Range range, Color color, int lineType = 0)
     {
         throw new System.NotImplementedException();
     }
+
+    //Services:
+    // + IServiceGrid
+    // + IServiceGridVisual
+    // + IServicePathfinding
+    // + IServiceLoSandCover
 }
