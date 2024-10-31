1898cf221b9f846ae06dbfc8070a7a3f628b2ce4
TheBretonnian <tomasmacias@outlook.com>
2024-10-31 22:33:36 +0100

    Fix and extend Tile

--- Assets/Scripts/NewArch/Grid System/ITileVisual.cs
+++ Assets/Scripts/NewArch/Grid System/ITileVisual.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public interface ITileVisual
 {
+    
     void Highlight(Color color);
-
     void Outline(Color color);
 
+    void ClearHighlight();
+    void ClearOutline();
     void Reset();
 }
--- Assets/Scripts/NewArch/Grid System/Tile.cs
+++ Assets/Scripts/NewArch/Grid System/Tile.cs
@@ -4,13 +4,13 @@ public class Tile : MonoBehaviour, ITile, ITileVisual, IPathfindingNode , ISelec
 {
     //Private fields
     ITileVisual tileVisual;
+    bool isWalkableTerrain;
     
     //Public Properties (ITile)
     public Vector2Int LocalCoordinates{ get; private set;} //Shared by ITile and IPathfindingNoe
     public IUnit Unit {get; set;}
     
     //Public Properties (IPathfinding)
-    public bool IsWalkable { get; set;}
     public IPathfindingNode CameFrom { get; set; }
     public bool IsInZoC { get; set; }
     public int ZoCPenalty { get; set; }
@@ -26,26 +26,18 @@ public class Tile : MonoBehaviour, ITile, ITileVisual, IPathfindingNode , ISelec
     {
         LocalCoordinates = localCoordinates;
         //Set default values
-        IsWalkable = true;
+        isWalkableTerrain = true;
         IsInZoC = false;
         ZoCPenalty = 0;
     }
 
-    //Public method ITileVisual
-    public void Highlight(Color color)
-    {
-        tileVisual.Highlight(color); //Act as wrapper or facade for tileVisuals
-    }
+    //Public method ITileVisual: Act as wrapper or facade for tileVisuals
+    public void Highlight(Color color) => tileVisual.Highlight(color);
+    public void Outline(Color color) => tileVisual.Outline(color);
 
-    public void Outline(Color color)
-    {
-        tileVisual.Outline(color);
-    }
-
-    public void Reset()
-    {
-        tileVisual.Reset();
-    }
+    public void ClearHighlight() =>  tileVisual.ClearHighlight();
+    public void ClearOutline() =>  tileVisual.ClearOutline();
+    public void Reset() => tileVisual.Reset();
 
     //Public methods IPathfinding
     public void UpdateFCost()
@@ -56,10 +48,10 @@ public class Tile : MonoBehaviour, ITile, ITileVisual, IPathfindingNode , ISelec
     public bool IsWalkable()
     {
         //Implement walkable check rules here like:
-        return IsWalkable || IsOccupied(); 
+        return isWalkableTerrain && !IsOccupied(); 
     }
 
-    public bool IsOccupied() => (Unit != null);
+    public bool IsOccupied() => Unit != null;
 
     public void Selected()
     {
