cd204016cbe4dd3ecd2979788feab127e5226485
TheBretonnian <67970930+TheBretonnian@users.noreply.github.com>
2025-02-13 23:11:23 +0100

    Create TerrainDescriptor.cs
    Place holder for editing terrain features in Editor. Not needed if terrain features are defined in a data container or file (JSON), but definitely useful for prototyping.
    The game objects (or prefabs) containing this component can be placed as children of Grid GameObject (containing Grid manager or TerrainBuilder) for easy finding during Grid Creation.

---
+++ Assets/Scripts/NewArch/Terrain/TerrainDescriptor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+//This class is a place holder for TileData, 
+// to be able to define terrain feature with GameObjects in Editor.
+// After Grid Creation using stamdard prefab, the TileData 
+// is copied into corresponding Tile.
+// Afterwards, the game object this script belongs to is removed
+// if RemoveAfterInit is true.
+public class TerrainDescriptor : MonoBehavior
+{
+  public TileData TileData;
+  public bool RemoveAfterInit = false;
+}
