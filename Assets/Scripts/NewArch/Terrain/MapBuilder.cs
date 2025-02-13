dccaf09bf628484192ae87207a38e53d3dbb2d2c
TheBretonnian <67970930+TheBretonnian@users.noreply.github.com>
2025-02-13 23:37:04 +0100

    Update MapBuilder.cs
    Add implementation

--- Assets/Scripts/NewArch/Terrain/MapBuilder.cs
+++ Assets/Scripts/NewArch/Terrain/MapBuilder.cs
@@ -9,13 +9,28 @@ public class MapBuilder : MoboBehaviour
      GridManager?.OnGridCreated+=UpdateMap;
    }
 
-  void UpdateMap()
-  {
-    //Get all TerrainDescriptor components in children
-    //For each TerrainDescriptor:
-    //  Get ITile in world coordinates using transform.position of TerrainDescriptor's game object
-    //  If Valid ITile, cast to Tile
-    //  Set Tile.TileData with TerrainDescriptor.TileData
-    //  If TerrainDescriptor.RemoveAfterInit, destroy game object of TerrainDescriptor
-  }
+void UpdateMap()
+{
+    // Get all TerrainDescriptor components in children
+    TerrainDescriptor[] terrainDescriptors = GetComponentsInChildren<TerrainDescriptor>();
+    // use for loop instead since destroying game object may affect Iterator next
+    for (int i=0; i<terrainDescriptors.Length; i++)
+    {
+        descriptor = terrainDescriptors[i];
+        // Get ITile in world coordinates using transform.position of TerrainDescriptor's game object
+        ITile tile = gridManager.Grid.GetElement(gridManager.Grid.WorldToLocal(descriptor.transform.position));
+        
+        if (tile is Tile targetTile)
+        {
+            // Set Tile.TileData with TerrainDescriptor.TileData
+            targetTile.TileData = descriptor.TileData;
+
+            // If TerrainDescriptor.RemoveAfterInit, destroy game object of TerrainDescriptor
+            if (descriptor.RemoveAfterInit)
+            {
+                Destroy(descriptor.gameObject);
+            }
+        }
+    }
+}
 }
